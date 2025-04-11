5ad67dbf87aae2a35f1ccf6c0c4ca1c2eff3d317
moshebeutel <moshebeutel@gmail.com>
2025-04-11 12:51:38 +0300

    update sweep

--- vision/GEP/sweep_script.sh
+++ vision/GEP/sweep_script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "Running perp lr 0.01..."
+echo "Running perp lr 0.01 clip0 0.01"
 echo "Start time: $(date +'%T')"
-python3 ./vision/GEP/main.py -p --perp --lr 0.01 --n_epoch 25
+python3 ./vision/GEP/main.py -p --perp --lr 0.01 --n_epoch 25 --clip0 0.01
 
-echo "Running no perp lr 0.01..."
+echo "Running no perp lr 0.01 clip0 0.01"
 echo "Start time: $(date +'%T')"
-python3 ./vision/GEP/main.py -p --lr 0.01 --n_epoch 25
+python3 ./vision/GEP/main.py -p --lr 0.01 --n_epoch 25 --clip0 0.01
 
-echo "Running perp lr 0.1 clip0 0.1"
+echo "Running perp lr 0.1 clip0 0.01"
 echo "Start time: $(date +'%T')"
-python3 ./vision/GEP/main.py -p --perp --lr 0.1 --n_epoch 25 --clip0 0.1
+python3 ./vision/GEP/main.py -p --perp --lr 0.1 --n_epoch 25 --clip0 0.01
 
-echo "Running no perp lr 0.1 clip0 0.1"
+echo "Running no perp lr 0.1 clip0 0.01"
 echo "Start time: $(date +'%T')"
-python3 ./vision/GEP/main.py -p --lr 0.1 --n_epoch 25 --clip0 0.1
+python3 ./vision/GEP/main.py -p --lr 0.1 --n_epoch 25 --clip0 0.01
 echo "End time: $(date +'%T')"
